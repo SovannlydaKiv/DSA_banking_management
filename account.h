c93cbaf313641b3624fc60ae1ff1626911446ef8
Kiv Sovannlyda <sovannlydakiv@gmail.com>
2026-06-18 21:17:35 +0700

    initial push (main needs changes)

---
+++ account.h
@@ -0,0 +1,32 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Account{
+    int accID;              //id of user here
+    string accName;         //name of the account owner
+    string username;        //username for display
+    string password;        //users password
+    double balance;         //users balance
+};
+
+struct Node{
+    Account data;
+    Node* next;
+};
+
+struct List{
+    int n;
+    Node* head;
+    Node* tail;
+};
+
+List* createList();
+
+void addAcc(List* ls, int id, string name, string username, string password, double bal);
+void display(List* ls);
+
+#endif
