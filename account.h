ba495f7e68f795462a949b0acf8a3ce2afc74970
Kiv Sovannlyda <sovannlydakiv@gmail.com>
2026-06-22 18:27:13 +0700

    fixed issues related to logging in with no acc and such

--- account.h
+++ account.h
@@ -5,23 +5,39 @@ using namespace std;
 struct Account{
     int accID;              //id of user here
     string accName;         //name of the account owner
-    string username;        //username for display
     string password;        //users password
     double balance;         //users balance
 };
 
+struct Transaction{
+    int transID;
+    int fromAcc;
+    int toAcc;
+    int type;       //what type of transaction, is it deposit or withdrawal or transfer
+    double amount;
+};
+
 struct Node{
+    int type;
     Account data;
+    Transaction transData;
     Node* next;
 };
 
 struct List{
     int n;
+    int transCounter;
     Node* head;
     Node* tail;
 };
 
 List* createList();
 
-void addAcc(List* ls, int id, string name, string username, string password, double bal);
+void addAcc(List* ls, int id, string name, string password, double bal);
+void addTrans(List* ls, int fromAcc, int toAcc, int type, double amount);
+void deleteAcc(List* ls, int id);
+void updateAcc(List* ls, int id);
+void searchAcc(List* ls, int id);
+int login(List* ls, int id, string password);
+bool accExists(List* ls, int id);
 void display(List* ls);
